b15bb3f26eb8034a44eb700e569afddc096fb1c8
Sebastian Alberski <sebastian.alberski@lumesse.com>
2015-05-26 16:51:57 +0200

    Przygotowanie konfiguracji do obslugi batch'y

--- monitor-app/src/main/java/org/developers/monitor/app/config/AppConfig.java
+++ monitor-app/src/main/java/org/developers/monitor/app/config/AppConfig.java
@@ -1,13 +1,16 @@
 package org.developers.monitor.app.config;  
   
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
 @ComponentScan("org.developers.monitor") 
 @EnableWebMvc
 @EnableAutoConfiguration
+@EnableBatchProcessing
 public class AppConfig {  
 
 }  
