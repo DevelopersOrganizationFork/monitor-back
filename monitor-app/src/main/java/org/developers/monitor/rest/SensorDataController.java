d5fadaef9bff3fa83bd50ccbe06f5dafa05dc586
Silwest <tyskie3>
2015-06-02 10:28:09 -0500

    Added thread to handle data from active mq, added configuration how many items should be taken each time from mq

--- monitor-app/src/main/java/org/developers/monitor/rest/SensorDataController.java
+++ monitor-app/src/main/java/org/developers/monitor/rest/SensorDataController.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import javax.jms.JMSException;
+import org.developers.sensor.configuration.ConfigLoader;
 import org.developers.sensor.jms.consumer.JMSConnection;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Silwest
  */
+@Component
 @RestController
 @RequestMapping("/rest/sensordata")
 public class SensorDataController {
+    private static final String  NUMBER_OF_ITEMS = "NumberOfItems";
+    
+    private final JMSConnection jmsConnnection = new JMSConnection();
+    final int numberOfItems = Integer.parseInt(ConfigLoader.properties.getProperty(NUMBER_OF_ITEMS));
 
-    private JMSConnection jmsConnnection = new JMSConnection();
-
+    
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<JsonNode> getSensorData() throws Exception {
         List<JsonNode> result = new ArrayList<>();
@@ -31,5 +40,16 @@ public class SensorDataController {
             result.add(actualObj);
         }
         return result;
+    }
+
+    @Scheduled(fixedRate = 15000) // 15s
+    public void getDataFromMq() throws IOException, JMSException{
+        List<JsonNode> result = new ArrayList<>();
+        for (int i = 0; i < numberOfItems; i++) {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode actualObj = mapper.readTree(jmsConnnection.getMessage());
+            result.add(actualObj);
+        }
+        System.out.println(result.toString());
     }
 }
--- monitor-app/src/main/java/org/developers/sensor/jms/consumer/JMSConnection.java
+++ monitor-app/src/main/java/org/developers/sensor/jms/consumer/JMSConnection.java
@@ -14,7 +14,6 @@ import org.developers.sensor.configuration.ConfigLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
 
 public class JMSConnection {
 
